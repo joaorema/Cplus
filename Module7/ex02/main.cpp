dc967973f80e3f06526530a9a87c5a024a2bce86
joaorema <joaopcrema@gmail.com>
2025-09-09 12:58:49 +0100

    Added extra test for module 7. Fixed some Makefiles

--- Module7/ex02/main.cpp
+++ Module7/ex02/main.cpp
@@ -9,7 +9,28 @@ int main()
 {
     {
         Array<int> one(5);              //starting array of ints with 5 nbrs inside
+        Array<int> two(5);
+        two = one;
         std::cout << GREEN"First Test : Array int"RESET << std::endl;
+        std::cout << "Lets Print Array one" << std::endl;
+        for(int i = 0; i < one.size(); i++)
+        {
+            std::cout << one[i] << std::endl;
+        }
+        std::cout << GREEN"Lets check if copy worked"RESET << std::endl;
+        for (int i = 0; i < two.size(); i++)
+        {
+            try
+            {
+                std::cout << two[i] << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                std::cout << "Invalid Index" << std::endl;
+            }
+            
+        }
+        std::cout << GREEN"Lets apply some changes to array one"RESET << std::endl;
         for (int i = 0; i < one.size(); i++)
         {
             try
@@ -22,6 +43,28 @@ int main()
                 std::cout << "Invalid Index" << std::endl;
             }
         }
+        two = one;
+        std::cout << GREEN"Lets copy after updating array"RESET << std::endl;
+        for (int i = 0; i < two.size(); i++)
+        {
+            try
+            {
+                std::cout << two[i] << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                 std::cout << "Invalid Index" << std::endl;
+            }
+            
+        }
+        std::cout << "Lets change one value in first array and check if changes the copy" << std::endl;
+        {
+            int i = 1;
+            one[i] = 50;
+            std::cout << "Value in Array one in [i] : " << one[i] << std::endl;
+            std::cout << "Value in Array two in [i] : " << two[i] << std::endl;
+        }
+
     }
     {
         Array<double> two(3);              //starting array of ints with 5 nbrs inside
@@ -41,8 +84,8 @@ int main()
     }
     {
         Array<std::string> text(5);
-        std::cout << GREEN"Third Test : Array strings"RESET << std::endl;
-        for(int i = 0; i < text.size(); i++)
+        std::cout << GREEN"Third Test : Array strings (Lets check one invalid index)"RESET << std::endl;
+        for(int i = 0; i < text.size() + 1; i++)
         {
             try
             {
@@ -51,7 +94,7 @@ int main()
             }
             catch(std::exception &e)
             {
-                std::cout << "Invalid Index" << std::endl;
+                std::cout << RED"Invalid Index"RESET << std::endl;
             }
         }
     }
